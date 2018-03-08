ac9b520328b339302e97047747f2cbe5417d8c0d
Ben Lopatin <ben@benlopatin.com>
2018-03-08 15:34:10 -0500

    Bump version: 0.1.0 → 0.2.0

--- woopra_tracker/__init__.py
+++ woopra_tracker/__init__.py
@@ -64,7 +64,7 @@ from __future__ import unicode_literals
 
 __author__ = """Ben Lopatin"""
 __email__ = 'ben@benlopatin.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __license__ = 'MIT'
 
 import logging
